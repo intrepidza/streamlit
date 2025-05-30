54f25d31c9c89aeffb94f62a00f6eb55a339c857
T H <timthezombie@gmail.com>
2025-05-30 17:06:50 +0200

    Updatez4

--- streamlit_test.py
+++ streamlit_test.py
@@ -2,8 +2,10 @@ import streamlit as st
 import pandas as pd
 
 test = pd.DataFrame(
-    pd.Series(['a', 1, 'c'], name='val1', dtype=str),
-    pd.Series(['d', 2, 'e'], name='val2', dtype=str),
+    [
+        pd.Series(['a', 1, 'c'], name='val1', dtype=str),
+        pd.Series(['d', 2, 'e'], name='val2', dtype=str),
+    ]
 )
 
 # test = test['val1']
