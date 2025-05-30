05bc75a626eb03c439a5c5e5ddf3b480c6c368ee
intrepidza <106755381+intrepidza@users.noreply.github.com>
2025-05-30 17:08:50 +0200

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -5,6 +5,7 @@ test = pd.DataFrame(
     [
         pd.Series(['a', 1, 'c'], name='val1', dtype=str),
         pd.Series(['d', 2, 'e'], name='val2', dtype=str),
+        pd.Series(['f', 2, 'h'], name='val2', dtype=str),
     ]
 )
 
